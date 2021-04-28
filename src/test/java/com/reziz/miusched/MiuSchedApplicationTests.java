6813060217fcf3e2a92ed130e990ecae9b5a2db1
Abdulaziz Ali <rediabdulaziz@gmail.com>
2021-04-28 07:54:32 -0500

    init

---
+++ src/test/java/com/reziz/miusched/MiuSchedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reziz.miusched;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiuSchedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
