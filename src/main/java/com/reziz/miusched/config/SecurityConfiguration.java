34ebbc1aa47f53fb5ec3d11e4234310fa0cb2e83
Abdulaziz Ali <rediabdulaziz@gmail.com>
2021-05-09 11:28:27 -0500

    add cors configuration

--- src/main/java/com/reziz/miusched/config/SecurityConfiguration.java
+++ src/main/java/com/reziz/miusched/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.reziz.miusched.filter.AuthenticationConfigConstants;
 import com.reziz.miusched.filter.JWTAuthenticationFilter;
 import com.reziz.miusched.filter.JWTAuthorizationFilter;
 import com.reziz.miusched.service.AuthenticationUserDetailService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -37,5 +43,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(authenticationUserDetailService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://127.0.0.1:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
